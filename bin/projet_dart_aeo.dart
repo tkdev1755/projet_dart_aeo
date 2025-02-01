001a27f5a628cce82a2b778bdffddac395dbb8aa
tkdev1755 <khetibh@gmail.com>
2025-02-01 10:42:12 +0100

    Initial commit

---
+++ bin/projet_dart_aeo.dart
@@ -0,0 +1,5 @@
+import 'package:projet_dart_aeo/projet_dart_aeo.dart' as projet_dart_aeo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projet_dart_aeo.calculate()}!');
+}
---
+++ test/projet_dart_aeo_test.dart
@@ -0,0 +1,8 @@
+import 'package:projet_dart_aeo/projet_dart_aeo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
